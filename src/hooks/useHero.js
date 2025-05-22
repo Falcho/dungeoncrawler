5d729e624ba007dbe53494b2de64535b7a978f43
MojoOno <Ffranck0406@gmail.com>
2025-05-22 11:18:19 +0200

    Updated useHero hook to use localPersist and proper export

--- src/hooks/useHero.js
+++ src/hooks/useHero.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { persistence } from "../utils/persistence";
+import persistence from "../utils/localPersistence";
 
 const useHero = () => {
     const HERO_ENDPOINT = "/hero";
     
-    const [ hero, setHero ] = useState(null);
+    const [hero, setHero] = useState(null);
 
     const fetchHero = () => {
         persistence.fetchData(HERO_ENDPOINT)
@@ -14,7 +14,7 @@ const useHero = () => {
             .catch((error) => {
                 console.error("Error fetching hero:", error);
             });
-    }
+    };
 
     const updateHero = (hero) => {
         persistence.saveData(HERO_ENDPOINT, hero)
@@ -24,18 +24,28 @@ const useHero = () => {
             .catch((error) => {
                 console.error("Error updating hero:", error);
             });
-    }
+    };
 
     const deleteHeroById = (id) => {
-        persistence.deleteData(HERO_ENDPOINT + "/" + id)
+        persistence.deleteData(`${HERO_ENDPOINT}/${id}`)
             .then(() => {
                 setHero(null);
             })
             .catch((error) => {
                 console.error("Error deleting hero:", error);
             });
-    }
-    
+    };
 
+    useEffect(() => {
+        fetchHero();
+    }, []);
 
-}
+    return {
+        hero,
+        fetchHero,
+        updateHero,
+        deleteHeroById,
+    };
+};
+
+export default useHero;
