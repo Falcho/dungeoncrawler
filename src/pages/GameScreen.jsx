bf423b64c6bdb6751544e8fa2730c441bc293604
MojoOno <Ffranck0406@gmail.com>
2025-05-22 15:34:53 +0200

    auto format + changed hero to character

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -12,7 +12,6 @@ import autoBattler from "../utils/autobattler";
 import caveEntrace from "../assets/goblin-cave/Goblin-cave-entrance.png";
 import treasureRoom from "../assets/goblin-cave/Goblin-cave-treasureRoom.png";
 
-
 const monster = {
   id: 1,
   name: "Goblin",
@@ -38,7 +37,7 @@ const dungeon = {
       image: caveEntrace,
       name: "Entrance",
       description: "the entrance to the cave.",
-      monsters: [{...monster}],
+      monsters: [{ ...monster }],
       exits: [{ direction: "north", roomId: 2 }],
     },
     {
@@ -46,7 +45,7 @@ const dungeon = {
       image: treasureRoom,
       name: "Treasure Room",
       description: "a room filled with treasure.",
-      monsters: [{...monster}],
+      monsters: [{ ...monster }],
       exits: [],
     },
   ],
@@ -128,9 +127,7 @@ export default function GameScreen() {
 
       case "autoBattle":
         setBattleLog([...battleLog, "Resolving battle..."]);
-        updateHero(
-          autoBattler(hero, currentRoom.monsters[0], addToBattleLog)
-        );
+        updateHero(autoBattler(hero, currentRoom.monsters[0], addToBattleLog));
         setGameState("battleOutcome");
         break;
 
@@ -185,7 +182,7 @@ export default function GameScreen() {
       <div className={styles.content}>
         <div className={styles.sidebar}>
           <div className={styles.characterInfoBox}>
-            <CharacterInfo hero={hero} loot={loot} />
+            <CharacterInfo character={hero} loot={loot} />
           </div>
           <div className={styles.dungeonMapBox}>
             <DungeonMap />
