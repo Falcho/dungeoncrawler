735a6cf63502243e75ac2bd923b4a65060b84b37
MojoOno <Ffranck0406@gmail.com>
2025-05-22 15:33:03 +0200

    added a div tag

--- src/components/BattleScreen.jsx
+++ src/components/BattleScreen.jsx
@@ -34,7 +34,7 @@ const BattleScreen = ({ gameState, currentRoom, character, monster }) => {
         </div>
       </div>
 
-      <div>
+      <div className={styles.infoText}>
         <h1>Current Room: {currentRoom?.name}</h1>
         <h2>Current State: {gameState}</h2>
         {gameState === "enterRoom" && (
