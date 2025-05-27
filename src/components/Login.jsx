88c8111679c78ad70e7fcb45ee731cdc9dc121f7
AndyTheDragon <andre.jansam@gmail.com>
2025-05-27 22:17:19 +0200

    feat: add navigation to registration page in Login component and improve button styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import styles from './Login.module.css';
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
 
-function Login ({ login }) {
-  const init = { username: '', password: '' };
+function Login({ login }) {
+  const init = { username: "", password: "" };
+  const navigate = useNavigate();
   const [loginCredentials, setLoginCredentials] = useState(init);
 
   const performLogin = (evt) => {
@@ -39,9 +41,15 @@ function Login ({ login }) {
         <button className={styles.button} type="submit">
           Login
         </button>
+        <button
+          className={styles.button}
+          onClick={() => navigate("/register")}
+          type="button"
+        >
+          Don't have an account? Register
+        </button>
       </form>
     </div>
   );
 }
 export default Login;
-
