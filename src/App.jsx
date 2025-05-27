fd0cbd843cdf2d65616ad240ff6624c36996ce6c
AndyTheDragon <andre.jansam@gmail.com>
2025-05-27 22:17:42 +0200

    feat: add routes for login and register pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import NewCharacter from './pages/NewCharacter';
 import GameScreen from './pages/GameScreen';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StartPage />} />
+        <Route path="/login" element={<StartPage />} />
         <Route path="/new-character" element={<NewCharacter />} />
         <Route path="/game" element={<GameScreen />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<div> 404 Not found </div>} />
       </Routes>
     </Router>
   );
